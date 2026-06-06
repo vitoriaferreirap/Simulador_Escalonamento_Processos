423719c00528c479280be3b833465d583514ce04
vitoriaferreirap <vitoriaferreira@alunos.utfpr.edu.br>
2026-06-06 15:37:12 -0300

    refactor: alterando classe processo, atributos, contrutor e getters e setter, simplificando e entendendo classe.

--- model/Processo.java
+++ model/Processo.java
@@ -1,5 +1,43 @@
 package model;
 
 public class Processo {
-    //R epresenta cada processo/thread
+    // Representa cada processo, que será gerênciado pelo escalonador
+    private int id;
+    private int tempoExecucaoTotal; // tempo total que o processo PRECISA na CPU
+    private int tempoExecutado; // quanto tempo o processo GASTOU rodando
+    private EstadoProcesso estadoAtual;
+
+    public Processo(int id, int tempoExecucaoTotal, EstadoProcesso estadoAtual) {
+        this.id = id;
+        this.tempoExecucaoTotal = tempoExecucaoTotal;
+        this.tempoExecutado = 0; // todo processo comça sem tempo gasto
+        this.estadoAtual = estadoAtual.NOVO; // todo processo inicia no estado padrao de NOVO
+    }
+
+    // metodo para simular a execução do processo por um determinado tempo - quantum
+
+    // getters e setters
+    public int getId() {
+        return id;
+    }
+
+    public int getTempoExecucaoTotal() {
+        return tempoExecucaoTotal;
+    }
+
+    public int getTempoExecutado() {
+        return tempoExecutado;
+    }
+
+    public EstadoProcesso getEstadoAtual() {
+        return estadoAtual;
+    }
+
+    // univo set
+    public void setEstadoAtual(EstadoProcesso estadoAtual) {
+        this.estadoAtual = estadoAtual;
+    }
+
+    // metodo de exibição de resultados
+
 }
